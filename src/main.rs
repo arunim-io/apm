fe99ffe7348f8610b9a0b9d891e4f2f46a4a74f3
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-01-03 01:01:54 +0600

    add gtk-layer-shell support

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use gtk::{glib::ExitCode, prelude::*, Application, ApplicationWindow, Text};
+use gtk_layer_shell::{Layer, LayerShell};
 
 fn main() -> ExitCode {
     let app = Application::builder()
@@ -9,6 +10,11 @@ fn main() -> ExitCode {
         let window = ApplicationWindow::new(app);
 
         window.set_child(Some(&Text::builder().text("Hello, World!").build()));
+
+        window.init_layer_shell();
+        window.set_layer(Layer::Overlay);
+        window.set_exclusive_zone(-1);
+
         window.present();
     });
 
