cf4df22a6e137650e5766abbc10053613829ff91
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-01-03 00:40:19 +0600

    add gtk4 support

--- default.nix
+++ default.nix
@@ -1,10 +1,13 @@
-{ rustPlatform, lib }:
+{ rustPlatform, lib, pkg-config, glib, gtk4, gtk4-layer-shell }:
 let
   manifest = (lib.importTOML ./Cargo.toml).package;
 in
 rustPlatform.buildRustPackage {
+  inherit (manifest) version;
+
   pname = manifest.name;
-  version = manifest.version;
   cargoLock.lockFile = ./Cargo.lock;
   src = lib.cleanSource ./.;
+
+  buildInputs = [ pkg-config glib gtk4 gtk4-layer-shell ];
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::{glib::ExitCode, prelude::*, Application, ApplicationWindow, Text};
+
+fn main() -> ExitCode {
+    let app = Application::builder()
+        .application_id("dev.github.arunim-io.apm")
+        .build();
+
+    app.connect_activate(|app| {
+        let window = ApplicationWindow::new(app);
+
+        window.set_child(Some(&Text::builder().text("Hello, World!").build()));
+        window.present();
+    });
+
+    app.run()
 }
+
