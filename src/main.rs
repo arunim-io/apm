d756ee172b6b8dc58d400a41b1266d3fcb0dd31c
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-01-03 14:32:35 +0600

    add fullscreen support & close on escape key press

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
-use gtk::{glib::ExitCode, prelude::*, Application, ApplicationWindow, Text};
-use gtk_layer_shell::{Layer, LayerShell};
+use gtk::{self, gdk, glib, prelude::*};
+use gtk_layer_shell::{Edge, LayerShell};
 
-fn main() -> ExitCode {
-    let app = Application::builder()
+fn main() -> glib::ExitCode {
+    let app = gtk::Application::builder()
         .application_id("dev.github.arunim-io.apm")
         .build();
 
     app.connect_activate(|app| {
-        let window = ApplicationWindow::new(app);
+        let window = gtk::ApplicationWindow::new(app);
 
-        window.set_child(Some(&Text::builder().text("Hello, World!").build()));
+        window.set_child(Some(&gtk::Label::new(Some("Hello, World!"))));
 
         window.init_layer_shell();
-        window.set_layer(Layer::Overlay);
+        window.set_layer(gtk_layer_shell::Layer::Overlay);
         window.set_exclusive_zone(-1);
+
+        let controller = gtk::EventControllerKey::new();
+        controller.connect_key_pressed(|_, key, _, _| {
+            if let gdk::Key::Escape = key {
+                std::process::exit(0);
+            }
+            glib::Propagation::Proceed
+        });
+        window.add_controller(controller);
+        window.set_fullscreened(true);
+        window.set_keyboard_mode(gtk_layer_shell::KeyboardMode::Exclusive);
+        for edge in vec![Edge::Top, Edge::Bottom, Edge::Left, Edge::Right] {
+            window.set_anchor(edge, true);
+        }
 
         window.present();
     });
