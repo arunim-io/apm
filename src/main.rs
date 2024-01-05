b959b8aeed616111b9a763bb1467d862784156fc
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-01-05 15:58:43 +0600

    show list of text with icons

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::path::Path;
 use gtk::{self, gdk, glib, prelude::*};
 use gtk_layer_shell::{Edge, LayerShell};
 
+static GAPS: i32 = 25;
+
 fn main() -> glib::ExitCode {
     let app = gtk::Application::builder()
         .application_id("dev.github.arunim-io.apm")
@@ -22,8 +24,6 @@ fn main() -> glib::ExitCode {
     app.connect_activate(|app| {
         let window = gtk::ApplicationWindow::new(app);
 
-        window.set_child(Some(&gtk::Label::new(Some("Hello, World!"))));
-
         window.init_layer_shell();
         window.set_layer(gtk_layer_shell::Layer::Overlay);
         window.set_exclusive_zone(-1);
@@ -41,6 +41,23 @@ fn main() -> glib::ExitCode {
         for edge in vec![Edge::Top, Edge::Bottom, Edge::Left, Edge::Right] {
             window.set_anchor(edge, true);
         }
+
+        let list = gtk::Box::new(gtk::Orientation::Horizontal, GAPS);
+        list.set_halign(gtk::Align::Center);
+        for i in vec![1, 2, 3] {
+            let label = gtk::Label::new(Some(&i.to_string()));
+            let icon = gtk::Image::from_file(Path::new("examples/shutdown.png"));
+
+            let container = gtk::Box::new(gtk::Orientation::Vertical, 10);
+            container.set_valign(gtk::Align::Center);
+
+            container.append(&icon);
+            container.append(&label);
+
+            list.append(&container);
+        }
+
+        window.set_child(Some(&list));
 
         window.present();
     });
