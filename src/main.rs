95ca62bf8833af6b96ce9acf0fb2c5e46ccce5cc
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-01-06 23:50:30 +0600

    move code into one function
    I will organise the code once it is ready.

--- src/main.rs
+++ src/main.rs
@@ -25,24 +25,18 @@ fn main() -> glib::ExitCode {
         .application_id("dev.github.arunim-io.apm")
         .build();
 
-    app.connect_activate(activate(config));
-    app.connect_startup(startup);
-    app.run()
-}
+    app.connect_startup(|_| {
+        let provider = gtk::CssProvider::new();
+        provider.load_from_path(Path::new("examples/styles.css"));
 
-fn startup(_app: &gtk::Application) {
-    let provider = gtk::CssProvider::new();
-    provider.load_from_path(Path::new("examples/styles.css"));
+        gtk::style_context_add_provider_for_display(
+            &gdk::Display::default().expect("Could not connect to a display."),
+            &provider,
+            gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+        );
+    });
 
-    gtk::style_context_add_provider_for_display(
-        &gdk::Display::default().expect("Could not connect to a display."),
-        &provider,
-        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
-    );
-}
-
-fn activate(config: Config) -> impl Fn(&gtk::Application) {
-    move |app| {
+    app.connect_activate(move |app| {
         let window = gtk::ApplicationWindow::new(app);
 
         window.init_layer_shell();
@@ -68,42 +62,37 @@ fn activate(config: Config) -> impl Fn(&gtk::Application) {
             .halign(gtk::Align::Center)
             .build();
 
-        config
-            .clone()
-            .buttons
-            .into_iter()
-            .for_each(|button| container.append(&get_button(button)));
+        config.clone().buttons.into_iter().for_each(|data| {
+            let label = gtk::Label::new(Some(&data.label));
+            let icon = gtk::Image::builder()
+                .file(data.icon)
+                .width_request(100)
+                .height_request(100)
+                .build();
+
+            let container = gtk::Box::builder()
+                .orientation(gtk::Orientation::Vertical)
+                .spacing(25)
+                .valign(gtk::Align::Center)
+                .margin_top(50)
+                .margin_end(50)
+                .margin_start(50)
+                .margin_bottom(50)
+                .build();
+            container.append(&icon);
+            container.append(&label);
+
+            let button = gtk::Button::builder()
+                .valign(gtk::Align::Center)
+                .child(&container)
+                .build();
+            button.connect_clicked(move |_| println!("{}", data.cmd));
+            container.append(&button);
+        });
 
         window.set_child(Some(&container));
         window.present();
-    }
-}
+    });
 
-fn get_button(data: Button) -> gtk::Button {
-    let label = gtk::Label::new(Some(&data.label));
-    let icon = gtk::Image::builder()
-        .file(data.icon)
-        .width_request(100)
-        .height_request(100)
-        .build();
-
-    let container = gtk::Box::builder()
-        .orientation(gtk::Orientation::Vertical)
-        .spacing(25)
-        .valign(gtk::Align::Center)
-        .margin_top(50)
-        .margin_end(50)
-        .margin_start(50)
-        .margin_bottom(50)
-        .build();
-    container.append(&icon);
-    container.append(&label);
-
-    let button = gtk::Button::builder()
-        .valign(gtk::Align::Center)
-        .child(&container)
-        .build();
-    button.connect_clicked(move |_| println!("{}", data.cmd));
-
-    return button;
+    app.run()
 }
