362e06df6dbccdc28634ccb8322aebde6c22d998
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-01-07 12:37:55 +0600

    switch to rust nightly with fenix

--- flake.nix
+++ flake.nix
@@ -6,21 +6,33 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  nixConfig = {
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
   };
 
   outputs = inputs@{ flake-parts, systems, nixpkgs, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
     systems = import systems;
 
-    perSystem = { pkgs, system, config, ... }: {
-      packages = {
-        abar = config.packages.default;
-        default = pkgs.callPackage ./default.nix { };
+    perSystem = { pkgs, system, config, inputs', ... }:
+      let
+        inherit (inputs'.fenix.packages.complete) cargo rustc rust-src;
+      in
+      {
+        packages = {
+          abar = config.packages.default;
+          default = pkgs.callPackage ./default.nix { rustPlatform = pkgs.makeRustPlatform { inherit cargo rustc; }; };
+        };
+        devShells.default = with pkgs; mkShell {
+          inputsFrom = [ config.packages.default ];
+          RUST_SRC_PATH = "${rust-src}/lib/rustlib/src/rust/library";
+        };
       };
-      devShells.default = with pkgs; mkShell {
-        inputsFrom = [ config.packages.default ];
-        nativeBuildInputs = [ rustc cargo gcc rustfmt clippy ];
-        RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
-      };
-    };
   };
 }
